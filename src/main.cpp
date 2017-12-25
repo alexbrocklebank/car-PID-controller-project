89bcc91cb96e878af39a74e4f52de90aa8ada959
alexbrocklebank <alexbrocklebank@gmail.com>
2017-12-25 17:31:13 -0500

    Steering Angle set to degrees

--- src/main.cpp
+++ src/main.cpp
@@ -71,7 +71,7 @@ int main()
           * another PID controller to control the speed!
           */
 		  pid.UpdateError(cte);
-		  steer_value = pid.TotalError();
+		  steer_value = rad2deg(pid.TotalError());
 
 		  // DEBUG
 		  std::cout << " Steering Value: " << steer_value << std::endl;
